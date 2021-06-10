655dc44b5df47b626176c19d55b61c90ecb80a02
Adrienne Hein <adrienne.hein@gmail.com>
2021-06-09 21:47:54 -0700

    Add items todo list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import { render } from 'react-dom';
+import React from 'react';
 import './App.css';
 // import Todolist from './components/Todolist';
 
 export default function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, text: "Wash dishes", done: false },
     { id: 2, text: "Do laundry", done: false },
     { id: 3, text: "Take shower", done: false }
-  ];
+  ]);
+  
   return (
     <div className="App">
       <h1>Checklist</h1>
       <Todolist todos={todos}/>
+      <AddTodo setTodos={setTodos} />
     </div>
   );
 }
 
+// //object destructuring on props object: props > {todos}
 
-//object destructuring on props object: props > {todos}
-function Todolist({todos}) {
+function Todolist({todos})  {
   return (
     <ul>
-      {todos.map(todo=> (
+      { todos.map(todo => (
         <li key={todo.id}>{todo.text}</li>
-  
       ))}
-
     </ul>
   )
 }
+
+function AddTodo( {setTodos} ) {
+  const inputRef = React.useRef();
+  function handleSubmit(e) {
+    e.preventDefault();
+    const text = e.target.elements.addTodo.value;
+    const todo = {
+      id: 4,
+      text,
+      done: false,
+    };
+    setTodos( prevTodos => {
+      return prevTodos.concat(todo);
+    });
+    inputRef.current.value = '';
+
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="addTodo">Add Item</label>
+      <input id="addTodo" ref={inputRef} />
+      <button type="submit">Add</button>
+    </form>
+  )
+}
+
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,6 +1,17 @@
+// class component example
+
 import React from 'react';
 
 class Todolist extends React.Component  {
+    render()  {
+        return(
+            <ul>
+                {this.props.todos.map( todo => (
+                    <li key={todo.id}>{todo.text}</li>
+                ))}
+            </ul>
+        )
+    }
 }
 
 
